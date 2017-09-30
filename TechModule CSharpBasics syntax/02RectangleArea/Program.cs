0aba06e26182f523df87e6275b63ee89b6906b7e
Martin <martin.tasev@abv.bg>
2017-09-30 16:24:38 +0300

    Added first homework to repo

---
+++ TechModule CSharpBasics syntax/02RectangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = width * height;
+
+            Console.WriteLine("{0:f2}", area);
+        }
+    }
+}
---
+++ TechModule CSharpBasics syntax/04BeverageLabels/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energyContent = double.Parse(Console.ReadLine());
+            double sugarContent = double.Parse(Console.ReadLine());
+
+            double energy = (volume / 100)* energyContent;
+            double sugar = (volume/100)* sugarContent;
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars",energy,sugar);
+        }
+    }
+}
